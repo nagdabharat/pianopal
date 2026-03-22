3bcc87b6d62c5629c4b4a791831732677719d83f
nagdabharat <nagdabharat@gmail.com>
2026-03-21 21:10:07 -0700

    Updates

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "pianopal-v1";
+
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (new URL(e.request.url).hostname === "api.anthropic.com") {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
